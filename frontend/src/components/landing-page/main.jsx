a5b12a5adcd351470ce118b1413a0ca017cd0cbd
ashshadiqihasbi-hue <ashshadiqihasbi@gmail.com>
2026-08-05 17:03:42 +0700

    fix bug

--- frontend/src/components/landing-page/main.jsx
+++ frontend/src/components/landing-page/main.jsx
@@ -4,8 +4,8 @@ import News from './News.jsx'
 import '../../styles/landing-page/landing.css'
 import '../../styles/landing-page/news-section.css'
 
-Main() {
-  return (
+function Main() {
+    return (
     <main>
       <Home />
       <Edukasi />
