62a00d7c45a545ec3d047f7ccfe703ae205f56d9
Lindo Alvindro <lindoalvindro@gmail.com>
2026-08-07 02:35:57 +0700

    style: update AdminDashboard table column headers to HARIAN, MINGGUAN, BULANAN, DETAIL

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -273,10 +273,10 @@ const AdminDashboard = () => {
                     <th>SEKOLAH</th>
                     <th>KELAS</th>
                     <th>EMAIL</th>
-                    <th>TOTAL HARIAN</th>
-                    <th>WEEKLY</th>
-                    <th>MONTHLY</th>
-                    <th style={{ width: '60px', textAlign: 'center' }}>ACTION</th>
+                    <th>HARIAN</th>
+                    <th>MINGGUAN</th>
+                    <th>BULANAN</th>
+                    <th style={{ width: '70px', textAlign: 'center' }}>DETAIL</th>
                   </tr>
                 </thead>
                 <tbody>
