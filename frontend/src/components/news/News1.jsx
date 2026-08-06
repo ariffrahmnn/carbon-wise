b7b73e90767a5f1baed74f42375d6dfad545e5a5
unknown <carbonwisepotato@gmail.com>
2026-08-06 21:59:43 +0700

    test berita

--- frontend/src/components/news/News1.jsx
+++ frontend/src/components/news/News1.jsx
@@ -17,7 +17,7 @@ export default function News1() {
 
       {/* MAIN ARTICLE CARD */}
       <main className="news-content-card">
-        <article className="news-article">
+        <e className="news-article">
           
           {/* Judul Utama Artikel */}
           <h2 className="article-title">
